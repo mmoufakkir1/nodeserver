98da43f2f08553c2be4ee186ce97558a3419fcac
mmoufakkir <mmoufakkir>
2019-05-20 14:15:30 -0400

    added commnet for the db credientials

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://@cluster0-pmbm2.mongodb.net/test?retryWrites=true');
+mongoose.connect('mongodb+srv://user:pwd@cluster0-pmbm2.mongodb.net/test?retryWrites=true');
